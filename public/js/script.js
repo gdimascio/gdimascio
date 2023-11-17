67d276afebda3bbe36bea93edf48eb335992d5d0
gdimascio <guido.dimascio@gmail.com>
2023-11-17 20:27:51 -0300

    burger links

--- public/js/script.js
+++ public/js/script.js
@@ -9,3 +9,12 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function burger_menu(){
+    var x = document.getElementById("_burger-links");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
