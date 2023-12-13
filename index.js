3542173e324f5bc3454bf2d86deaf9a87f8317ef
gdimascio <guido.dimascio@gmail.com>
2023-12-13 11:51:57 -0300

    vercel deploy

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const hbs = require('hbs');
 const app = express();
 
 // HBS
+app.set('views', __dirname + '/../views');
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + "/views/partials");
 
