2ed41697449be76c9b0d146d80c82b61ad9e8dd1
gdimascio <guido.dimascio@gmail.com>
2023-12-13 11:28:51 -0300

    vercel deploy

--- index.js
+++ index.js
@@ -18,20 +18,20 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: false}));
 
 
-// app.use('/', router)
-app.get("/", (req, res) => {
-    const htmlResponse = `
-    <html>
-        <head>
-        <title>NodeJs y Express en Vercel</title>
-        </head>
-        <body>
-        <h1>Soy un proyecto Back end en vercel</h1>
-        </body>
-    </html>
-    `;
-    res.send(htmlResponse);
-});
+app.use('/home', router)
+// app.get("/", (req, res) => {
+//     const htmlResponse = `
+//     <html>
+//         <head>
+//         <title>NodeJs y Express en Vercel</title>
+//         </head>
+//         <body>
+//         <h1>Soy un proyecto Back end en vercel</h1>
+//         </body>
+//     </html>
+//     `;
+//     res.send(htmlResponse);
+// });
 
 
 
